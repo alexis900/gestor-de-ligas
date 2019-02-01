ef14ff5139a991f73555959a3adddfd1b8ce87e4
alexis900 <alexandermpf77@gmail.com>
2019-02-02 00:27:21 +0100

    Update funciona

--- update_ronda.php
+++ update_ronda.php
@@ -1,6 +1,21 @@
 <?php
+require_once("db.php");
 echo "<pre>";
 print_r($_GET);
 echo "</pre>";
 
-$sql = "update ;";
+foreach($_GET as $get => $partido){
+    foreach ($partido as $partidos => $valor) {
+        $partidoId = $partidos;
+    }
+}
+
+$pt1 = (!empty($_GET['partido'][$partidoId]['p1'])) ? $_GET['partido'][$partidoId]['p1'] : "null";
+$pt2 = (!empty($_GET['partido'][$partidoId]['p2'])) ? $_GET['partido'][$partidoId]['p2'] : "null";
+$fechac = $_GET['partido'][$partidoId]['date'] . " " . $_GET['partido'][$partidoId]['time'];
+echo $sql = "update partido set puntos1 = $pt1, puntos2 = $pt2, fecha = '$fechac' where id = $partidoId";
+if (mysqli_query($con, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($con);
+}
--- ver_ronda.php
+++ ver_ronda.php
@@ -18,7 +18,7 @@ $title = $nombreLiga . " - Ronda $rondaId";
 head($title);
 echo "<h2>$title</h2>";
 
-//Selecciona la informació que necessitarem
+// Selecciona la información que necessitaremos
 $sql = "select p.id, e1.nombre, e2.nombre, puntos1, puntos2, fecha from partido p 
 inner join equipo e1 on e1.id = p.equipo1_id 
 inner join equipo e2 on e2.id = p.equipo2_id 
@@ -42,7 +42,7 @@ while($row = mysqli_fetch_row($rst)){
 if ($par > 0) {
 
 ?>
-
+<!-- Comienza la tabla -->
     <table>
         <tr>
             <th>Equipo 1</th>
@@ -50,19 +50,22 @@ if ($par > 0) {
             <th>Equipo 2</th>
             <?php
 
+            //Si hay alguna fecha disponible, se mostrará la columna
             $mostrarFecha = false;
             for($i = 0; $i < $par;$i++){
                 if ($partido[$i]['fecha'] != null) {
                     $mostrarFecha = true;
                 }
             }
+
             if($mostrarFecha){
                 echo "<th>Fecha del partido</th>";
             }
             ?>
         </tr>
         <?php
-
+        
+       //S
         if (!isset($_GET['partido'])) {
             for ($i=0; $i < $par; $i++) { 
                 echo "<tr>";
@@ -85,29 +88,37 @@ if ($par > 0) {
 
         $p1 = $partido[$partidoId]['p1'] == null ? null : $partido[$partidoId]['p1'] ;
         $p2 = $partido[$partidoId]['p2'] == null ? null : $partido[$partidoId]['p2'] ;
-        $f = $partido[$partidoId]['fecha'] == null ? null : $partido[$partidoId]['fecha'] ;
+
+        $fecha_completa = $partido[$partidoId]['fecha'] == null ? null : $partido[$partidoId]['fecha'] ;
+        if ($fecha_completa != null) {
+            $fecha = date_format(date_create($fecha_completa), 'Y-m-d');
+            $hora = date_format(date_create($fecha_completa), 'H:i:s');
+        }
+
         echo "<tr>";
             echo "<td>" . $partido[$partidoId]['e1'] . "</td>";
             if ($p1 > 0) {
                 echo "<td>" . $p2 . "</td>";
                 echo "<input type=\"hidden\" name=\"partido[$partId][p1]\" id=\"p1h\ value=\"$p1\"/>";
             } else {
-                echo "<td><input type=\"number\" name=\"partido[$partId][p1]\" id=\"p1\" value=\"0\" min=\"0\" max=\"100\"></td>";
+                echo "<td><input type=\"number\" name=\"partido[$partId][p1]\" id=\"p1\" min=\"0\" max=\"100\"></td>";
             }
 
             if ($p2 > 0) {
                 echo "<td>" . $p1 . "</td>";
                 echo "<input type=\"hidden\" name=\"partido[$partId][p2]\" id=\"p1h\ value=\"$p2\"/>";
             } else {
-                echo "<td><input type=\"number\" name=\"partido[$partId][p2]\" id=\"p2\" value=\"0\" min=\"0\" max=\"100\"></td>";
+                echo "<td><input type=\"number\" name=\"partido[$partId][p2]\" id=\"p2\" min=\"0\" max=\"100\"></td>";
             }
             echo "<td>" . $partido[$partidoId]['e2'] . "</td>";
 
-            if ($f == null) {
+            if ($fecha_completa == null) {
                 echo "<td><input type=\"date\" name=\"partido[$partId][date]\" id=\"date\" value=\"". date('Y-m-d') . "\"></td>";
                 echo "<td><input type=\"time\" name=\"partido[$partId][time]\" id=\"time\" value=\"". date("H:i") ."\"></td>";
             } else {
-                echo "<td>$f</td>";
+                echo "<td>$fecha_completa</td>";
+                echo "<input type=\"hidden\" name=\"partido[$partId][date]\" value=\"$fecha\">";
+                echo "<input type=\"hidden\" name=\"partido[$partId][time]\" value=\"$hora\">";
             }
 
             if($p1 != null && $p2 != null && $fecha == null){
